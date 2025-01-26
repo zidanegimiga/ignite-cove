51313fb86da59c668c1ab86b0da378f5af995c00
ZidaneGimiga <zidane@nathandigital.com>
2025-01-26 06:58:13 +0300

    chore: configure navigation

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import CustomSplash from '@/components/CustomSplash'
+
+const LoadingScreen = () => {
+  return (
+      <CustomSplash />
+  )
+}
+
+export default LoadingScreen
+
+const styles = StyleSheet.create({})
