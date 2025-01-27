0471cc0fce8da770372fa26136a3eb6b1ef3ad19
ZidaneGimiga <zidane@nathandigital.com>
2025-01-27 05:18:07 +0300

    feat: complete payment loading screen

--- components/features/Payment/PaymentLoader.tsx
+++ components/features/Payment/PaymentLoader.tsx
@@ -1,14 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { useFonts } from "expo-font";
+import { ThemedText } from "@/components/ThemedText";
 
 const PaymentLoader = () => {
+  const [loaded, error] = useFonts({
+    "Oswald-Bold": require("@/assets/fonts/oswald/Oswald-Bold.ttf"),
+    "Oswald-Light": require("@/assets/fonts/oswald/Oswald-Light.ttf"),
+  });
   return (
-    <View>
-      <Text>PaymentLoader</Text>
+    <View style={{ flex: 1, paddingBottom: 48 }}>
+      <ThemedText
+        style={{
+          fontFamily: "Oswald-Bold",
+          fontSize: 18,
+          lineHeight: 24,
+          marginTop: 4,
+          color: "#484646",
+        }}
+      >Processing Payment...</ThemedText>
     </View>
-  )
-}
+  );
+};
 
-export default PaymentLoader
+export default PaymentLoader;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
